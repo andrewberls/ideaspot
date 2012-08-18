53b7205c44c0f89ee7d8b86fb6dfa5a8f7f393fc
Phaedrus Raznikov <phraznikov@gmail.com>
2012-08-18 14:48:47 -0700

    Got basic cookie authentication implemented

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,46 +1,37 @@
 class PollsController < ApplicationController
 
-  # before :check_authentication
-
   def new
     @poll = Poll.new
   end
-
 
   def create
     @poll = Poll.new(params[:poll])
 
     if @poll.save
+      cookies[:"authenticated_#{@poll.pin}"] = true
       return redirect_to @poll
     else
-      # Error occured - TODO: validate client side
       return render :new
     end
   end
 
   def show
     @poll = Poll.find(params[:id])
+    return redirect_to root_url unless cookies[:"authenticated_#{@poll.pin}"]
   end
 
   def join
     if @poll = Poll.find_by_pin(params[:pin])
+      cookies[:"authenticated_#{@poll.pin}"] = true
       redirect_to show_poll_path(@poll)
     else
       redirect_to root_path
-      flash[:error] = "woah there cowboy"
+      flash[:error] = "Ooops, that PIN didn't work. Try again?"
     end
   end
-
 
   def destroy
     @poll = Poll.find(params[:id])
     @poll.destroy!
   end
-
-
-  # private
-
-  # def check_authentication
-  #   return redirect_to root_url unless cookies[:authenticated]
-  # end
 end
