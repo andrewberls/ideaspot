9d79dd3aa33115b224a22bb5db16440817f07bad
Phaedrus Raznikov <phraznikov@gmail.com>
2012-08-17 22:01:18 -0700

    Got rid of errant putss

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -3,7 +3,6 @@ class Poll < ActiveRecord::Base
   attr_accessible :pin, :title
 
   def generate_pin
-    puts "*"*50
     digest_string = self.id.to_s << Time.now.to_i.to_s
     self.pin = Digest::MD5.hexdigest(digest_string)[0..3]
   end
